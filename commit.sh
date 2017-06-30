b2f46aa3f6120a4ec0c6ca7d9d9e329a5a14c695
chenhaoxiang <chenhaoxiang0117@qq.com>
2017-06-30 22:11:45 +0800

    20170611【Cocos2d-x】开发基础-Node与Node层级架构

--- commit.sh
+++ commit.sh
@@ -1,8 +1,8 @@
 ﻿#!/bin/sh
 rake generate 
-echo '已经编译成最新的页面'
+echo 已经编译成最新的页面
 rake deploy
-echo '已将本地的静态网站部署到github'
+echo 已将本地的静态网站部署到github
 git add .
 echo 请输入本次提交的注释 如输入空白字符则使用"commit",倒计时600s
 read -t 600 var
@@ -15,5 +15,5 @@ echo commit is $var
 git commit -am "$var"
 fi
 git push origin source
-echo '已提交代码至source分支'
+echo 已提交代码至source分支
 sleep 5
